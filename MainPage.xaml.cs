4041db24f52db007092475c0de6280e531ef6066
koziolek-ksiadz <pantalon2819@gmail.com>
2025-04-14 17:23:55 +0200

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,163 @@
+﻿using ksiazka.Models;
+using System.Linq;
+
+namespace ksiazka
+{
+    public partial class MainPage : ContentPage
+    {
+        public List<ContactsBook> Contacts { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            ContactsBook.InitializeTestContacts();
+            Contacts = ContactsBook.contacts; 
+            Contacts = Contacts.OrderBy(c => c.LastName).ThenBy(c => c.Name).ToList();
+            contactsListView.ItemsSource = Contacts;
+        }
+
+        // przycisk do dodawania kontaktu
+        private void OnAddContactClicked(object sender, EventArgs e)
+        {
+            AddContact();
+        }
+
+        // przycisk do usuwania kontaktu
+        private void OnDeleteClicked(object sender, EventArgs e)
+        {
+            var button = sender as Button; // przy bledzie w rzutowaniu wyrzuci null zamiast wyjatku
+
+            // zwraca przycisk ktory jest przpisany do kontaktu
+            var contact = button?.BindingContext as ContactsBook;
+
+            if (contact != null)
+            {
+                RemoveContact(contact);
+            }
+        }
+
+
+        private void RemoveContact(ContactsBook contact)
+        {
+            ContactsBook.RemoveContact(contact); 
+            Contacts = ContactsBook.contacts.OrderBy(c => c.LastName).ThenBy(c => c.Name).ToList();
+            contactsListView.ItemsSource = null;
+            contactsListView.ItemsSource = Contacts;
+        }
+
+
+
+        private void OnEditClicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            var contact = button?.BindingContext as ContactsBook;
+
+            if (contact != null)
+            {
+                //znajduje label konkretnego a nie wszystkie
+                var nameLabel = button.Parent.FindByName<Label>("nameLabel");
+                var nameEdit = button.Parent.FindByName<Entry>("nameEdit");
+
+                var lastNameLabel = button.Parent.FindByName<Label>("lastNameLabel");
+                var lastNameEdit = button.Parent.FindByName<Entry>("lastNameEdit");
+
+                var phoneNumLabel = button.Parent.FindByName<Label>("phoneNumLabel");
+                var phoneNumEdit = button.Parent.FindByName<Entry>("phoneNumEdit");
+
+                var editButton = button.Parent.FindByName<Button>("editButton");
+
+                bool isEditing = nameEdit.IsVisible;
+
+                if (isEditing)
+                {
+                    contact.Name = nameEdit.Text;
+                    contact.LastName = lastNameEdit.Text;
+                    contact.PhoneNumber = phoneNumEdit.Text;
+
+                    ContactsBook.EditContact(contact, new ContactsBook
+                    {
+                        Id = contact.Id,
+                        Name = contact.Name,
+                        LastName = contact.LastName,
+                        PhoneNumber = contact.PhoneNumber,
+                        Age = contact.Age
+                    });  
+
+                    Contacts = ContactsBook.contacts.OrderBy(c => c.LastName).ThenBy(c => c.Name).ToList();
+                    contactsListView.ItemsSource = null;
+                    contactsListView.ItemsSource = Contacts;
+
+                    nameLabel.IsVisible = true;
+                    nameEdit.IsVisible = false;
+
+                    lastNameLabel.IsVisible = true;
+                    lastNameEdit.IsVisible = false;
+
+                    phoneNumLabel.IsVisible = true;
+                    phoneNumEdit.IsVisible = false;
+
+                    editButton.Text = "Edytuj";
+                }
+                else
+                {
+                    nameLabel.IsVisible = false;
+                    nameEdit.IsVisible = true;
+
+                    lastNameLabel.IsVisible = false;
+                    lastNameEdit.IsVisible = true;
+
+                    phoneNumLabel.IsVisible = false;
+                    phoneNumEdit.IsVisible = true;
+
+                    editButton.Text = "Zapisz";
+                    editButton.BackgroundColor = Color.FromArgb("22ff22");
+                }
+            }
+        }
+
+        private async void AddContact()
+        {
+            string name = await DisplayPromptAsync("Dodaj Kontakt", "Wpisz imię:", maxLength: 30);
+            string lastName = await DisplayPromptAsync("Dodaj Kontakt", "Wpisz nazwisko:", maxLength: 30);
+            string phoneNumber = await DisplayPromptAsync("Dodaj Kontakt", "Wpisz numer telefonu:", maxLength: 15);
+
+            if (!string.IsNullOrWhiteSpace(name) && !string.IsNullOrWhiteSpace(lastName) && !string.IsNullOrWhiteSpace(phoneNumber))
+            {
+                var newContact = new ContactsBook
+                {
+                    Id = Contacts.Count + 1,
+                    Name = name,
+                    LastName = lastName,
+                    PhoneNumber = phoneNumber,
+                    Age = 45
+                };
+
+                ContactsBook.AddContact(newContact);  
+                Contacts = ContactsBook.contacts.OrderBy(c => c.LastName).ThenBy(c => c.Name).ToList();
+                contactsListView.ItemsSource = null;
+                contactsListView.ItemsSource = Contacts;
+            }
+            else
+            {
+                await DisplayAlert("Błąd", "Proszę wpisać wszystkie wymagane dane.", "OK");
+            }
+        }
+
+
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string searchTerm = e.NewTextValue;
+
+
+            List<ContactsBook> results = ContactsBook.SearchContacts(searchTerm);
+            results = results.OrderBy(c => c.LastName).ThenBy(c => c.Name).ToList();
+
+            string message = results.Count > 0
+                ? string.Join("\n", results.Select(c => $"{c.Name} {c.LastName} - {c.PhoneNumber}"))
+                : "Brak wyników.";
+
+            contactsListView.ItemsSource = results;
+
+        }
+    }
+}
---
+++ Models/Contacts.cs
@@ -0,0 +1,60 @@
+﻿namespace ksiazka.Models
+{
+    using System.Collections.Generic;
+
+    public class ContactsBook
+    {
+        public static List<ContactsBook> contacts = new List<ContactsBook>();
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Age { get; set; }
+
+        // Metoda do dodania kontaktu
+        public static void AddContact(ContactsBook contact)
+        {
+            contacts.Add(contact);
+        }
+
+        // Metoda do usunięcia kontaktu
+        public static void RemoveContact(ContactsBook contact)
+        {
+            contacts.Remove(contact);
+        }
+
+        // Metoda do edytowania kontaktu
+        public static void EditContact(ContactsBook oldContact, ContactsBook newContact)
+        {
+            var index = contacts.IndexOf(oldContact);
+            if (index != -1)
+            {
+                contacts[index] = newContact;
+            }
+        }
+
+
+        public static List<ContactsBook> SearchContacts(string searchTerm)
+        {
+            var result = contacts
+                .Where(c => $"{c.Name.ToLower()} {c.LastName.ToLower()} {c.PhoneNumber.ToLower()}".Contains(searchTerm.ToLower()))
+                .ToList();
+            return result;
+        }
+        public static void InitializeTestContacts()
+        {
+            contacts.Add(new ContactsBook { Id = 1, Name = "Kamil", LastName = "Jankowski", PhoneNumber = "321-654-987" });
+            contacts.Add(new ContactsBook { Id = 2, Name = "Monika", LastName = "Szymańska", PhoneNumber = "654-987-321" });
+            contacts.Add(new ContactsBook { Id = 3, Name = "Łukasz", LastName = "Woźniak", PhoneNumber = "777-888-999" });
+            contacts.Add(new ContactsBook { Id = 4, Name = "Katarzyna", LastName = "Wiśniewska", PhoneNumber = "111-222-333" });
+            contacts.Add(new ContactsBook { Id = 5, Name = "Michał", LastName = "Wójcik", PhoneNumber = "444-555-666" });
+            contacts.Add(new ContactsBook { Id = 6, Name = "Zofia", LastName = "Kaczmarek", PhoneNumber = "777-888-999" });
+            contacts.Add(new ContactsBook { Id = 7, Name = "Tomasz", LastName = "Mazur", PhoneNumber = "222-333-444" });
+            contacts.Add(new ContactsBook { Id = 8, Name = "Alicja", LastName = "Dąbrowska", PhoneNumber = "333-444-555" });
+            contacts.Add(new ContactsBook { Id = 9, Name = "Jakub", LastName = "Kubiak", PhoneNumber = "666-777-888" });
+            contacts.Add(new ContactsBook { Id = 10, Name = "Magdalena", LastName = "Pawlak", PhoneNumber = "999-000-111" });
+
+        }
+    }
+}
